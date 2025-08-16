8997eef5c59471664b089915e485c8c4e815758a
migueldams <douanlmiguel@gmail.com>
2025-08-16 15:30:41 +0100

    version 1.0.2

--- backend/server.mjs
+++ backend/server.mjs
@@ -17,6 +17,17 @@ app.get('/',(req,res)=>{
 })
 
 
+export default app;
+
+
+
+
+
+
+
+
+
+
 // import { createServer } from 'node:http';
 // const server = createServer((req, res) => {
 //   res.writeHead(200, { 'Content-Type': 'text/plain' });
